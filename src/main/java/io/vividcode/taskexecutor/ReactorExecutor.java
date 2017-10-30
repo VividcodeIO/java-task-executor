cb548dfa10f42c7095ce91a8557b3657d7fc46c4
Fu Cheng <alexcheng1982@gmail.com>
2017-10-31 11:27:35 +1300

    Simplify Reactor implementation

--- src/main/java/io/vividcode/taskexecutor/ReactorExecutor.java
+++ src/main/java/io/vividcode/taskexecutor/ReactorExecutor.java
@@ -1,8 +1,6 @@
 package io.vividcode.taskexecutor;
 
 import java.util.concurrent.ThreadLocalRandom;
-import java.util.function.Function;
-import java.util.function.IntFunction;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
@@ -11,14 +9,10 @@ public class ReactorExecutor {
 
   public static void main(String[] args) {
     int numOfTasks = 10;
-    final Mono<?>[] tasks = Flux.range(0, numOfTasks).flatMap(index -> Mono.fromCallable(() -> {
+    Flux.range(0, numOfTasks).flatMap(index -> Mono.fromCallable(() -> {
       Thread.sleep(ThreadLocalRandom.current().nextInt(2000));
       System.out.println(String.format("[%s] Run %d", Thread.currentThread().getName(), index));
       return null;
-    }).publishOn(Schedulers.elastic())).toStream().toArray((IntFunction<Mono<?>[]>) Mono[]::new);
-    Flux.combineLatest((Function<Object[], Void>) result -> {
-      System.out.println("Done!");
-      return null;
-    }, tasks).blockLast();
+    }).publishOn(Schedulers.elastic())).blockLast();
   }
 }
